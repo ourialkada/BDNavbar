d01c082f3b650c5173afa1dcefaa8557ed64d9f7
oalkada@gmail.com <oalkada@gmail.com>
2024-09-05 19:32:51 -0400

    changes

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,17 +1,29 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
-var _readingTime = require("reading-time");
+var _readingTime = require('reading-time');
 
 var _readingTime2 = _interopRequireDefault(_readingTime);
 
-var _core = require("@hebcal/core");
+var _core = require('@hebcal/core');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 window.onload = function () {
+	var options23 = {
+		timeZone: 'America/Los_Angeles',
+		year: 'numeric',
+		month: 'numeric',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		second: 'numeric'
+	},
+	    formatter = new Intl.DateTimeFormat([], options23);
 
-	var today = new Date();
+	console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
+
+	var today = formatter.format(new Date());
 	var endofweek = 7 - today.getDay() - 1;
 
 	console.log(endofweek);
